773f6e03ac45adec95bdf97757ab4d294ba29030
Ankit Kumar Pandit <ankitp@geekyants.com>
2026-02-12 22:43:20 +0530

    fix vue-tsc build fail

--- src/router.ts
+++ src/router.ts
@@ -4,20 +4,17 @@ const routes: RouteRecordRaw[] = [
   {
     path: '/',
     name: 'home',
-    // @ts-expect-error - Vue SFC dynamic import
     component: () => import('./views/HomeView.vue'),
   },
   {
     path: '/meetups/:meetupId',
     name: 'meetup',
-    // @ts-expect-error - Vue SFC dynamic import
     component: () => import('./views/PreJoinView.vue'),
     props: true,
   },
   {
     path: '/meetups/:meetupId/room',
     name: 'meetup-room',
-    // @ts-expect-error - Vue SFC dynamic import
     component: () => import('./views/MeetupRoomView.vue'),
     props: true,
   },
